f6538684ba9dee802c8f8cf3e759bc6e5f015619
YerinKim929 <nirey929@gmail.com>
2023-04-05 15:41:24 +0900

    Initial Commit

---
+++ CapstoneProject/CapstoneProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneProjectApp.swift
+//  CapstoneProject
+//
+//  Created by Yerin Kim on 2023/04/05.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
